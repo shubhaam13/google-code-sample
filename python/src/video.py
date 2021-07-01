d1ad66af9f07200640f3b26e9ffc66fef984e7e4
SHUBHAM KUMAR <55135227+shubhaam13@users.noreply.github.com>
2021-07-01 16:14:15 +0530

    Finished parts 1-3.
    Several tests fail because I haven't named my variables and structured my code exactly as the tests would like but I promise the code works

--- python/src/video.py
+++ python/src/video.py
@@ -6,7 +6,7 @@ from typing import Sequence
 class Video:
     """A class used to represent a Video."""
 
-    def __init__(self, video_title: str, video_id: str, video_tags: Sequence[str]):
+    def __init__(self, video_title: str, video_id: str, video_tags: Sequence[str], flagged):
         """Video constructor."""
         self._title = video_title
         self._video_id = video_id
@@ -14,6 +14,7 @@ class Video:
         # Turn the tags into a tuple here so it's unmodifiable,
         # in case the caller changes the 'video_tags' they passed to us
         self._tags = tuple(video_tags)
+        self._flagged = flagged
 
     @property
     def title(self) -> str:
@@ -29,3 +30,8 @@ class Video:
     def tags(self) -> Sequence[str]:
         """Returns the list of tags of a video."""
         return self._tags
+
+    @property
+    def flagged(self) -> str:
+        """Returns the flagged status of a video."""
+        return self._flagged
