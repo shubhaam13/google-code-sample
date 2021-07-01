5087133e553bca7065e4bc8472bb318bca6ef326
SHUBHAM KUMAR <55135227+shubhaam13@users.noreply.github.com>
2021-07-01 16:08:39 +0530

    Passed 23 tests

--- python/src/video_playlist.py
+++ python/src/video_playlist.py
@@ -3,3 +3,22 @@
 
 class Playlist:
     """A class used to represent a Playlist."""
+    def __init__(self, name: str, videos, original_name):
+        self._name = name
+        self._videos = videos
+        self._original_name = original_name
+
+    @property
+    def get_name(self):
+        """The name of the playlist"""
+        return self._name
+    
+    @property
+    def get_videos(self):
+        """Videos in the playlist"""
+        return self._videos
+    
+    @property
+    def get_original_name(self):
+        """Original name"""
+        return self._original_name
